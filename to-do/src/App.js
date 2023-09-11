ff7a4e39ecc58dce63413b9cf7d4a7c497e37e44
taehyeong9210 <skytubu@naver.com>
2023-09-11 20:54:27 +0900

    23년 9월 11일

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,10 +1,46 @@
 import TodoTemplate from './directory/TodoTemplate';
 import TodoInsert from './directory/TodoInsert';
+import TodoList from './directory/TodoList';
+import { useState, useCallback, useRef } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들기',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
