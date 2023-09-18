c81665995614205df03db09e7bbed004223662e0
taehyeong9210 <skytubu@naver.com>
2023-09-18 20:37:28 +0900

    23년9월18일

---
+++ news/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=4405aaaf225e44afb2439a8bdb1f7d97',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
